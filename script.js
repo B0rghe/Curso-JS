f9234cebb7752ed96e7ebb14427d807c6483fb98
Alexis Borghetti <borghe.08@gmail.com>
2021-11-01 20:58:27 -0300

    Clase 1

---
+++ script.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Ingrese su nombre:");
+let apellido = prompt("Ingrese su apellido:");
+let anio = parseInt(prompt("Hola " + nombre + " " + apellido + "! Por favor ingresa el año de tu nacimiento:"));
+let resultado = (2021 - anio);
+
+alert("Vaya, parece que ya tienes " + resultado + " años, pero no importa, todos podemos estudiar Javascript!");
